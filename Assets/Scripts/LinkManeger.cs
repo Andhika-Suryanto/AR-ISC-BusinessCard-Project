aa72762971ea41dd6b9fe132911831b228c44200
Andhika-Suryanto <92772451+Andhika-Suryanto@users.noreply.github.com>
2024-03-26 15:19:51 +0700

    Release 1.0.0
    Version 1.0.0 :
    - Upload The Project to GitHub

---
+++ Assets/Scripts/LinkManeger.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LinkManeger : MonoBehaviour
+{
+    public void openLinkedin()
+	{
+		Application.OpenURL("https://www.linkedin.com/in/andhika-suryanto-961a4925a");
+	}
+	
+	public void openGithub()
+	{
+		Application.OpenURL("https://github.com/Andhika-Suryanto");
+	}
+	
+	public void openReddit()
+	{
+		Application.OpenURL("https://www.reddit.com/user/Acedia_Gremory");
+	}
+	
+	public void openDiscord()
+	{
+		Application.OpenURL("https://discordapp.com/users/1088541872915480686");
+	}
+	
+	public void openX()
+	{
+		Application.OpenURL("https://twitter.com/AhnDokja");
+	}
+	
+	public void openInstagram()
+	{
+		Application.OpenURL("https://www.instagram.com/ahn_dokja/");
+	}
+	
+}
